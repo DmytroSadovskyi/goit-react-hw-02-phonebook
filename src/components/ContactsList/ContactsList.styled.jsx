fc65e0af92253183da6c7e0e44f93fe467a6880d
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-04-07 18:18:58 +0300

    Update ContactsList.styled.jsx

--- src/components/ContactsList/ContactsList.styled.jsx
+++ src/components/ContactsList/ContactsList.styled.jsx
@@ -9,6 +9,8 @@ export const ContactItem = styled.li`
   border-radius: 5px;
   padding-top: 10px;
   padding-bottom: 10px;
+  padding-left: 5px;
+  padding-right: 5px;
 
   &:not(:last-of-type) {
     margin-bottom: 10px;
