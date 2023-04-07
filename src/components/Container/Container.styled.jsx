e30c8c1c319bea16df8b348912959c80fcc407b4
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-04-07 18:38:21 +0300

    Update Container.styled.jsx

--- src/components/Container/Container.styled.jsx
+++ src/components/Container/Container.styled.jsx
@@ -1,10 +1,8 @@
 import styled from '@emotion/styled';
 
 export const Div = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+  width: 310px;
+  margin: 0 auto;
   padding-right: 15px;
   padding-left: 15px;
 `;
